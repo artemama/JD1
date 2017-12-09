4ae7d0c4fde5089149ddb9a39b3007e585854a5f
Fhntv <said-kirill@rambler.ru>
2017-12-10 02:33:19 +0300

    no message

--- src/com/intexsoft/task21/TimeStringSumm.java
+++ src/com/intexsoft/task21/TimeStringSumm.java
@@ -3,8 +3,24 @@ package com.intexsoft.task21;
 public class TimeStringSumm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "¬ стандартную библиотеку Java входит пакет, специально предназначенный дл€ работы с регул€рными выражени€ми Ч java.util.regex.";
+		long startTime = System.currentTimeMillis();
 
+		for (int i = 0; i < 20; i++) {
+			str += str;
+		}
+		long timeSpent = System.currentTimeMillis();
+		long timeSpentFinal = timeSpent - startTime;
+		System.out.println("программа выполн€лась " + timeSpentFinal * 1000 + " миллисекунд");
+
+		long startTime1 = System.currentTimeMillis();
+		StringBuilder strbild = new StringBuilder();
+		for (int i = 0; i < 20000; i++) {
+			strbild.append(strbild);
+		}
+		long timeSpent1 = System.currentTimeMillis();
+		long timeSpentFinal1 = timeSpent1 - startTime1;
+		System.out.println("программа co StringBuilder выполн€лась " + timeSpentFinal1 + " миллисекунд(ы)");
 	}
 
 }
