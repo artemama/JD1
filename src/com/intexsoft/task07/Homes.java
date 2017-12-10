7bc3f6b61e4c7296a5bcc5185ee7e6999279b695
Fhntv <said-kirill@rambler.ru>
2017-12-10 22:44:00 +0300

    no message

--- src/com/intexsoft/task07/Homes.java
+++ src/com/intexsoft/task07/Homes.java
@@ -3,7 +3,7 @@ package com.intexsoft.task07;
 public class Homes {
 
 	public static void main(String[] args) {
-		int wight1 = 2;
+		int wight1 = 3;
 		int wight2 = 4;
 		int height1 = 5;
 		int height2 = 6;
@@ -11,27 +11,17 @@ public class Homes {
 		int landheit = 9;
 		if (wight1 + wight2 <= landheit && height1 + height2 <= landwght) {
 			System.out.println("Дома помещаются");
-
+		}
+		if (wight1 + wight2 <= landwght && height1 + height2 <= landheit) {
+			System.out.println("Дома помещаются");
+		}
+		if (wight1 + height2 <= landwght && height1 + wight2 <= landheit) {
+			System.out.println("Дома помещаются");
+		}
+		if (wight1 + height2 <= landheit && height1 + wight2 <= landwght) {
+			System.out.println("Дома помещаются");
 		} else {
-			if (wight1 + wight2 <= landwght && height1 + height2 <= landheit) {
-				System.out.println("Дома помещаются");
-
-			} else {
-				if (wight1 + height2 <= landwght && height1 + wight2 <= landheit) {
-					System.out.println("Дома помещаются");
-
-				} else {
-					if (wight1 + height2 <= landheit && height1 + wight2 <= landwght) {
-						System.out.println("Дома помещаются");
-
-					} else {
-						System.out.println("Дома не помещаются");
-
-					}
-
-				}
-
-			}
+			System.out.println("Дома не помещаются");
 
 		}
 
