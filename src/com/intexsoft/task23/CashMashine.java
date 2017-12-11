9da3b21948dcf64f14bdb9364dbdf8134f7421f1
Fhntv <said-kirill@rambler.ru>
2017-12-11 15:57:02 +0300

    no message

--- src/com/intexsoft/task23/CashMashine.java
+++ src/com/intexsoft/task23/CashMashine.java
@@ -11,23 +11,23 @@ public class CashMashine {
 		x100 = 5;
 		x50 = 10;
 		x20 = 20;
+	}
 
+	private static void askmoney() {
 		int requestCash = 1000;
 		if (requestCash % 10 != 0) {
 			System.out.println("Wrong sum, pleas enter correct sum");
-		}
-		boolean firstAnswer;
-		if (x100 * 100 + x50 * 50 + x20 * 20 >= requestCash) {
-			firstAnswer = true;
-			
-			
-		} else {
-			firstAnswer = false;
-			System.out.println("not enough money");
-			
-		}
-		
 
+			boolean firstAnswer;
+			if (x100 * 100 + x50 * 50 + x20 * 20 >= requestCash) {
+				firstAnswer = true;
+
+			} else {
+				firstAnswer = false;
+				System.out.println("not enough money");
+
+			}
+
+		}
 	}
-
 }
---
+++ src/workClassPVTnotForCheck/class11desemb/Test.java
@@ -0,0 +1,10 @@
+package workClassPVTnotForCheck.class11desemb;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
