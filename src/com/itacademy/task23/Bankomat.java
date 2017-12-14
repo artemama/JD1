4ba4d93ea8caea17fa19af222c9875499e55e28f
Fhntv <said-kirill@rambler.ru>
2017-12-14 22:42:49 +0300

    14-12

--- src/com/itacademy/task23/Bankomat.java
+++ src/com/itacademy/task23/Bankomat.java
@@ -5,8 +5,9 @@ public class Bankomat {
 	public static void main(String[] args) {
 		CashMashine CM1 = new CashMashine();
 
-		CM1.addMoney(20, 30, 25);
-		CM1.askMoney(1250);
+		CM1.addMoney(20, 30, 5);
+		CM1.askMoney(2050);
+		CM1.reqestMoney();
 
 	}
 
---
+++ src/com/itacademy/task24/Electronics.java
@@ -0,0 +1,13 @@
+package com.itacademy.task24;
+
+abstract class Electronics {
+	private String name;
+	abstract void cleaning();
+	abstract void washing();
+	void play() {
+		System.out.println("Play pleasure music");
+	};
+		
+	}
+
+
