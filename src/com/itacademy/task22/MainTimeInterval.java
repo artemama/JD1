cd10879fb8b7221c2a3f013a39c5cc71fb1a60af
Fhntv <said-kirill@rambler.ru>
2017-12-26 23:37:23 +0300

    no message

--- src/com/itacademy/task22/MainTimeInterval.java
+++ src/com/itacademy/task22/MainTimeInterval.java
@@ -1,24 +1,21 @@
 package com.itacademy.task22;
-/*
-public class MainTimeInterval {
-/*Задание 22
-	Создать класс и объекты описывающие промежуток времени. Сам промежуток
-	в классе должен задаваться тремя свойствами: секундами, минутами, часами.
-	Сделать методы для получения полного количества секунд в объекте, сравнения
-	двух объектов (метод должен работать аналогично compareTo в строках). Создать
-	два конструктора: получающий общее количество секунд, и часы, минуты и секунды
-	по отдельности. Сделать метод для вывода данных
-	
-	
-	public static void main(String[] args) {
-		TimeInterval obj1 = new TimeInterval(12,15,20); //Объект 1 
-		TimeInterval obj2 = new TimeInterval(15,20,36);  //Объект 2  (создавал через пергруженный конст
-		obj1.printAllTime(0);
-		obj2.printAllTime(0);
-		obj1.printDividedTime();
-		obj2.printDividedTime();
-		//obj2.compareTime();
-	}
 
+public class MainTimeInterval {
+	/*
+	 * Задание 22 Создать класс и объекты описывающие промежуток времени. Сам
+	 * промежуток в классе должен задаваться тремя свойствами: секундами, минутами,
+	 * часами. Сделать методы для получения полного количества секунд в объекте,
+	 * сравнения двух объектов (метод должен работать аналогично compareTo в
+	 * строках). Создать два конструктора: получающий общее количество секунд, и
+	 * часы, минуты и секунды по отдельности. Сделать метод для вывода данных
+	 */
+
+	public static void main(String[] args) {
+		TimeInterval obj1 = new TimeInterval(52145); // Объект 1
+		TimeInterval obj2 = new TimeInterval(15, 20, 36); // Объект 2 (создавал через пергруженный конст
+		System.out.println(obj2.printAllSecond());
+		System.out.println(obj1.printAllSecond());
+		System.out.println(obj1.compareTo(obj2));
+
+	}
 }
-	*/
--- src/com/itacademy/task22/TimeInterval.java
+++ src/com/itacademy/task22/TimeInterval.java
@@ -14,45 +14,32 @@ public class TimeInterval {
 	private int minut;
 	private int second;
 
-	public TimeInterval(int h, int m, int s) { // конструктор
-		hour = h;
-		minut = m;
-		second = s;
+	public TimeInterval(int hour, int minut, int second) { // конструктор 1
+		this.hour = hour;
+		this.minut = minut;
+		this.second = second;
 	}
 
-	/*
-	 * public TimeInterval(int allsec) { //конструктор 2 (пробовал сделать
-	 * перегруженный конструктор) second = allsec; }
-	 */
-
-	public void printAllTime(int allsec) { // метод для получения полного количества секунд
-		allsec = (hour * 3600) + (minut * 60) + second;
-		System.out.println("obj1 = " + allsec);
+	public TimeInterval(int second) { // конструктор 2 (пробовал сделать перегруженный конструктор)
+		super();
+		this.second = second;
 	}
 
-	//public void printAllTime() {
-	
+	public int printAllSecond() { // метод для получения полного количества секунд
+		int allsec = (hour * 3600) + (minut * 60) + second;
+		return allsec;
+	}
+
+	public boolean compareTo(TimeInterval allsec) {
+		if (this.printAllSecond() == allsec.printAllSecond()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public void printDividedTime(int nrObj) {
 		System.out.println(String.format("Object %s have %s hours, %s minuts, %s seconds", nrObj, hour, minut, second));
 	}
-	public static void main(String[] args) {
-		TimeInterval obj1 = new TimeInterval(12,15,20); //Объект 1 
-		TimeInterval obj2 = new TimeInterval(15,20,36);  //Объект 2  (создавал через пергруженный конст
-		obj1.printAllTime(0);
-		obj2.printAllTime(0);
-		obj1.printDividedTime(1);
-		obj2.printDividedTime(2);
-/*
-	public int compareTime() {
-		if (this.second >= this.returnAllSecond()) {
-			System.out.println("Object1 => Object2");
-		} else {
-			System.out.println("Object1 < Object2");
-		}
 
-		return this.second - this.returnAllSecond();
-
-	}*/
-
-}}
+}
