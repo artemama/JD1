14b9fa4faa625d4ce665fc51c0aff8c9a0eeda61
Fhntv <said-kirill@rambler.ru>
2017-12-27 17:25:45 +0300

    no message

--- src/com/itacademy/task28/ArrayListMaxVal.java
+++ src/com/itacademy/task28/ArrayListMaxVal.java
@@ -10,17 +10,19 @@ public class ArrayListMaxVal {
 		for (int i = 0; i < arrSize; i++) {
 			int x = (int) (Math.random() * 10 + 1);
 			marks.add(x);
-
 		}
 		System.out.println(marks);
-		System.out.println("Maximum mark is:" + Collections.max(marks));
+		// System.out.println("Maximum mark is:" + Collections.max(marks));
+		Iterator<Integer> iterator = marks.iterator();
+		int maxMark = 0;
+		while (iterator.hasNext()) {
+			int tmp = iterator.next();
+			if (maxMark < tmp) {
+				maxMark = tmp;
+			}
 
-		/*
-		 * int max = 0; for (int i = 0; i < marks.size(); i++) { if (marks.get(i) >=
-		 * max) { max = marks.get(i);
-		 */
+		}
+		System.out.println("Maximum mark is:" + maxMark);
 	}
-
-	// System.out.println("Maximum mark is:" + max);
 
 }
